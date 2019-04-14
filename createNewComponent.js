d09c9d455e70c48c0eda9f7bf9b907a5660451d2
marttee <eemeli.martti@yle.fi>
2019-04-14 14:20:44 +0300

    linted and fixed

--- createNewComponent.js
+++ createNewComponent.js
@@ -1,49 +1,48 @@
+'use strict';
 const fs = require('fs');
 
-const componentName = process.argv[2];
+const componentName = process.argv[2],
+  outputPath = process.argv[3]
+    ? process.argv[3]
+    : `${__dirname}/${componentName}`,
+  templatePath = `${__dirname}/template`;
 
-if (componentName) {
-  const templatePath = `${__dirname}/template`;
-  const outputPath = process.argv[3] ? `${process.argv[3]}/${componentName}` : `src/js/components/${componentName}`;
-  createComponent(templatePath, outputPath);
-} else {
-  console.log('Component name not defined. Usage: node createNewComponent.js [NAME] [PATH]')
+function outputFileName (fileName) {
+  return `${outputPath}/${fileName}`.replace('__COMPONENT__', componentName);
 }
 
-function copyTemplateFile(fileName) {
+function copyTemplateFile (fileName) {
+  // Make new component folder if not exists
+  if (!fs.existsSync(outputPath)) {
+    fs.mkdirSync(outputPath);
+  }
   return new Promise((resolve, reject) => {
-    //Make new component folder if not exists
-    if (!fs.existsSync(outputPath)) {
-      fs.mkdirSync(outputPath)
-    }
-
-    fs.copyFile(`${templatePath}/${fileName}`, `${outputFileName(fileName)}`, (error) => {
-      if (error) {
-        reject(error);
+    fs.copyFile(
+      `${templatePath}/${fileName}`,
+      `${outputFileName(fileName)}`,
+      (error) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(`Succesfull copied ${fileName}!`);
       }
-      resolve(`Succesfull copied ${fileName}!`)
-    });
+    );
   });
 }
 
-function createComponent(templatePath, outputPath) {
-  listFiles(templatePath)
-    .then((files) => {
-      const copyPromises = files.map((file) => (
-        copyTemplateFile(file).then(status => console.log(status))
-      ));
-      Promise.all(copyPromises).then(() => {
-        listFiles(outputPath).then((files) => {
-          const injectPromises = files.map((file) => (
-            injectFile(file).then(status => console.log(status))
-          ));
-          Promise.all(injectPromises).then(() => console.log('Done!'));
-        });
-      });
-    });
+function injectFile (fileName) {
+  return new Promise((resolve) => {
+    const file = fs.readFileSync(`${outputPath}/${fileName}`, 'utf8');
+    fs.writeFileSync(
+      `${outputPath}/${fileName}`,
+      file.replace(/__COMPONENT__/gu, componentName),
+      'utf8'
+    );
+    resolve(`Injected ${outputPath}/${fileName}`);
+  });
 }
 
-function listFiles(folder) {
+function listFiles (folder) {
   return new Promise((resolve, reject) => {
     fs.readdir(folder, (error, files) => {
       if (error) {
@@ -54,15 +53,25 @@ function listFiles(folder) {
   });
 }
 
-function injectFile(fileName) {
-  return new Promise((resolve) => {
-    const file = fs.readFileSync(`${outputPath}/${fileName}`, 'utf8');
-    const regex = /__COMPONENT__/g;
-    fs.writeFileSync(`${outputPath}/${fileName}`, file.replace(regex, componentName), 'utf8')
-    resolve(`Injected ${outputPath}/${fileName}`)
+function createComponent () {
+  listFiles(templatePath).then((files) => {
+    const copyPromises = files.map(file => (
+      copyTemplateFile(file).then(status => console.log(status))
+    ));
+    Promise.all(copyPromises).then(() => {
+      listFiles(outputPath).then((injectFiles) => {
+        const injectPromises = injectFiles.map(file => (
+          injectFile(file).then(status => console.log(status))
+        ));
+        Promise.all(injectPromises).then(() => console.log('Done!'));
+      });
+    });
   });
 }
 
-function outputFileName(fileName) {
-  return `${outputPath}/${fileName}`.replace('__COMPONENT__', componentName);
+if (componentName) {
+  createComponent();
+} else {
+  console.log('Component name not defined.');
+  console.log('Usage: node createNewComponent.js [NAME] [PATH]');
 }
